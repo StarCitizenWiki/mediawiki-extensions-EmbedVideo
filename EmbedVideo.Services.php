33872885a148c5b4eecfd46b4abbd4c2f8297774
Whiteknight <wknight8111@gmail.com>
2010-05-19 09:04:59 -0400

    reorder services list to be alphabetical. Add alias interia->interiavideo. Cleanup code pretend like we have nice formatting for TeacherTube

--- EmbedVideo.Services.php
+++ EmbedVideo.Services.php
@@ -5,49 +5,54 @@ $wgEmbedVideoServiceList = array(
     'dailymotion' => array(
         'url' => 'http://www.dailymotion.com/swf/$1'
     ),
+    'divshare' => array(
+        'url' => 'http://www.divshare.com/flash/video2?myId=$1',
+    ),
     'funnyordie' => array(
         'url' =>
             'http://www.funnyordie.com/v1/flvideo/fodplayer.swf?file='.
             'http://funnyordie.vo.llnwd.net/o16/$1.flv&autoStart=false'
     ),
-    'teachertube' => array(
-        'extern' =>
-            '<embed src="http://www.teachertube.com/embed/player.swf" ' .
-            '   width="470" ' .
-            '   height="275" ' .
-            '   bgcolor="undefined" ' .
-            '   allowscriptaccess="always" ' .
-            '   allowfullscreen="true" ' .
-            '   flashvars="file=http://www.teachertube.com/embedFLV.php?pg=video_$2' .
-            '&menu=false' .
-            '&frontcolor=ffffff&lightcolor=FF0000' .
-            '&logo=http://www.teachertube.com/www3/images/greylogo.swf' .
-            '&skin=http://www.teachertube.com/embed/overlay.swf volume=80' .
-            '&controlbar=over&displayclick=link' .
-            '&viral.link=http://www.teachertube.com/viewVideo.php?video_id=$2' .
-            '&stretching=exactfit&plugins=viral-2&viral.callout=none&viral.onpause=false"' .
-            '/>',
-        'default_width' => 490,
-        'default_height' => 295
-    ),
     'googlevideo' => array(
         'id_pattern'=>'%[^0-9\\-]%',
         'url' => 'http://video.google.com/googleplayer.swf?docId=$1'
     ),
-    'sevenload' => array(
-        'url' => 'http://page.sevenload.com/swf/en_GB/player.swf?id=$1'
+    'interiavideo' => array(
+        'url' => 'http://video.interia.pl/i/players/iVideoPlayer.05.swf?vid=$1',
+    ),
+    'interia' => array(
+        'url' => 'http://video.interia.pl/i/players/iVideoPlayer.05.swf?vid=$1',
     ),
     'revver' => array(
         'url' => 'http://flash.revver.com/player/1.0/player.swf?mediaId=$1'
     ),
+    'sevenload' => array(
+        'url' => 'http://page.sevenload.com/swf/en_GB/player.swf?id=$1'
+    ),
+    'teachertube' => array(
+        'extern' =>
+            '<embed src="http://www.teachertube.com/embed/player.swf" ' .
+                'width="470" ' .
+                'height="275" ' .
+                'bgcolor="undefined" ' .
+                'allowscriptaccess="always" ' .
+                'allowfullscreen="true" ' .
+                'flashvars="file=http://www.teachertube.com/embedFLV.php?pg=video_$2' .
+                    '&menu=false' .
+                    '&frontcolor=ffffff&lightcolor=FF0000' .
+                    '&logo=http://www.teachertube.com/www3/images/greylogo.swf' .
+                    '&skin=http://www.teachertube.com/embed/overlay.swf volume=80' .
+                    '&controlbar=over&displayclick=link' .
+                    '&viral.link=http://www.teachertube.com/viewVideo.php?video_id=$2' .
+                    '&stretching=exactfit&plugins=viral-2' .
+                    '&viral.callout=none&viral.onpause=false' .
+                '"' .
+            '/>',
+        'default_width' => 490,
+        'default_height' => 295
+    ),
     'youtube' => array(
         'url'=>'http://www.youtube.com/v/$1'
-    ),
-    'interiavideo' => array(
-        'url' => 'http://video.interia.pl/i/players/iVideoPlayer.05.swf?vid=$1',
-    ),
-    'divshare' => array(
-        'url' => 'http://www.divshare.com/flash/video2?myId=$1',
     ),
     'youtubehd' => array(
         'url'=>'http://www.youtube.com/v/$1&ap=%2526fmt%3D22',
