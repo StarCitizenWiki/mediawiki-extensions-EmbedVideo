9f903147bd3e45c5ddee87b0a7fb08e93de17437
Alexia E. Smith <washuu@gmail.com>
2014-09-03 22:58:45 -0500

    Experimental Twitch support.

--- EmbedVideo.services.php
+++ EmbedVideo.services.php
@@ -9,7 +9,7 @@
  *
  **/
 
-// Build services list (may be augmented in LocalSettings.php)
+//Build services list - Make modifications within this file.
 $wgEmbedVideoServiceList = array(
 	'bambuser' => array(
 		'embed'			=> '<iframe src="//embed.bambuser.com/broadcast/$1" width="$2" height="$3" frameborder="0"></iframe>',
@@ -22,7 +22,7 @@ $wgEmbedVideoServiceList = array(
 		'https_enabled'	=> true
 	),
 	'bing' => array(
-		'embed' => '<iframe src="//hub.video.msn.com/embed/$1" width="$2" height="$3" frameborder="0" scrolling="no" noscroll></iframe>',
+		'embed'			=> '<iframe src="//hub.video.msn.com/embed/$1" width="$2" height="$3" frameborder="0" scrolling="no" noscroll></iframe>',
 		'default_width'	=> 640,
 		'default_ratio'	=> 16 / 9,
 		'https_enabled'	=> true
@@ -46,13 +46,13 @@ $wgEmbedVideoServiceList = array(
 		'https_enabled'	=> false
 	),
 	'kickstarter' => array(
-		'embed' => '<iframe src="$1/widget/video.html" width="$2" height="$3" frameborder="0" allowfullscreen="true"></iframe>',
+		'embed'			=> '<iframe src="$1/widget/video.html" width="$2" height="$3" frameborder="0" allowfullscreen="true"></iframe>',
 		'default_width'	=> 640,
 		'default_ratio'	=> 16 / 9,
 		'https_enabled'	=> true
 	),
 	'metacafe' => array(
-		'embed'		=> '<iframe src="http://www.metacafe.com/embed/$1/" width="$2" height="$3" frameborder="0" allowFullScreen="true"></iframe>',
+		'embed'			=> '<iframe src="http://www.metacafe.com/embed/$1/" width="$2" height="$3" frameborder="0" allowFullScreen="true"></iframe>',
 		'default_width'	=> 640,
 		'default_ratio'	=> 16 / 9,
 		'https_enabled'	=> false
@@ -64,7 +64,7 @@ $wgEmbedVideoServiceList = array(
 		'https_enabled'	=> true
 	),
 	'rutube' => array(
-		'embed'	=> '<iframe src="//rutube.ru/play/embed/$1" width="$2" height="$3" frameborder="0" allowfullscreen="true"></iframe>',
+		'embed'			=> '<iframe src="//rutube.ru/play/embed/$1" width="$2" height="$3" frameborder="0" allowfullscreen="true"></iframe>',
 		'default_width'	=> 640,
 		'default_ratio'	=> 16 / 9,
 		'https_enabled'	=> true
@@ -103,6 +103,30 @@ $wgEmbedVideoServiceList = array(
 	'videomaten' => array(
 		'embed'			=> '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="$2" height="$3" id="videomat" align="middle"><param name="allowScriptAccess" value="sameDomain" /><param name="movie" value="http://89.160.51.62/recordMe/play.swf?id=$1" /><param name="loop" value="false" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" /><embed src="http://89.160.51.62/recordMe/play.swf?id=$1" loop="false" quality="high" bgcolor="#ffffff" width="$2" height="$3" name="videomat" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></object>',
 		'default_ratio'	=> 300 / 200,
+		'https_enabled'	=> false
+	),
+	'twitchchannel' => array(
+		'embed'			=> '<object id="live_embed_player_flash" type="application/x-shockwave-flash" width="$2" height="$3" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=$1">
+	<param name="allowFullScreen" value="true" />
+	<param name="allowScriptAccess" value="always" />
+	<param name="allowNetworking" value="all" />
+	<param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" />
+	<param name="flashvars" value="hostname=www.twitch.tv&channel=$1&auto_play=false&start_volume=100" />
+</object>',
+		'default_width'	=> 640,
+		'default_ratio'	=> 620 / 378,
+		'https_enabled'	=> false
+	),
+	'twitchvod' => array(
+		'embed'			=> '<object id="clip_embed_player_flash" type="application/x-shockwave-flash" width="$2" height="$3" data="http://www.twitch.tv/widgets/archive_embed_player.swf" bgcolor="#000000">
+	<param name="movie" value="http://www.twitch.tv/widgets/archive_embed_player.swf" />
+	<param name="allowScriptAccess" value="always" />
+	<param name="allowNetworking" value="all" />
+	<param name="allowFullScreen" value="true" />
+	<param name="flashvars" value="channel=$1&amp;auto_play=false&amp;start_volume=100&amp;chapter_id=$4" />
+</object>',
+		'default_width'	=> 640,
+		'default_ratio'	=> 620 / 378,
 		'https_enabled'	=> false
 	),
 	'vimeo' => array(
