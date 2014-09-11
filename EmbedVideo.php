5a940aaa102b2742050ac255d71b784f0a120477
Alexia E. Smith <washuu@gmail.com>
2014-09-11 14:13:50 -0500

    Sometimes I commit horrible sins by not testing the code I commit.

--- EmbedVideo.php
+++ EmbedVideo.php
@@ -24,7 +24,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'name'				=> 'EmbedVideo',
 	'author'			=> array('Jim R. Wilson', 'Andrew Whitworth', 'Alexia E. Smith'),
 	'url'				=> 'http://www.mediawiki.org/wiki/Extension:EmbedVideo',
-	'version'			=> '2.1.1',
+	'version'			=> '2.1.2',
 	'descriptionmsg'	=> 'embedvideo_description'
 );
 
