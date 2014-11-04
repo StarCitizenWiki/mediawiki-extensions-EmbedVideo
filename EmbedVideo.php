4abc67e2c23d936b9245a99c264e17cad5124433
Alexia E. Smith <washuu@gmail.com>
2014-11-04 12:51:26 -0600

    Version bump.

--- EmbedVideo.php
+++ EmbedVideo.php
@@ -24,7 +24,7 @@ $wgExtensionCredits['parserhook'][] = array(
 	'name'				=> 'EmbedVideo',
 	'author'			=> array('Jim R. Wilson', 'Andrew Whitworth', 'Alexia E. Smith'),
 	'url'				=> 'http://www.mediawiki.org/wiki/Extension:EmbedVideo',
-	'version'			=> '2.1.4',
+	'version'			=> '2.1.5',
 	'descriptionmsg'	=> 'embedvideo_description'
 );
 
