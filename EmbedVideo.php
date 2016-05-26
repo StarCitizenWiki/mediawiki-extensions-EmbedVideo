83c2175cdd6ba10850fea70952bbafa1b35a908e
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2016-05-26 23:42:52 +0200

    Update EmbedVideo.php
    * Add "other contributors" to the author array
    * Consistently switch to use __DIR__
    * Minor formatting change (remove tabs, switch to apostrophe)
    * Go https

--- EmbedVideo.php
+++ EmbedVideo.php
@@ -22,35 +22,38 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 define( 'EV_VERSION', '2.2.9' );
 
 $wgExtensionCredits['parserhook'][] = [
-	'path'				=> __FILE__,
-	'name'				=> 'EmbedVideo',
-	'author'			=> ['Jim R. Wilson', 'Andrew Whitworth', 'Alexia E. Smith'],
-	'url'				=> 'http://www.mediawiki.org/wiki/Extension:EmbedVideo',
-	'version'			=> EV_VERSION,
-	'descriptionmsg'	=> 'embedvideo_description',
-	'license-name'		=> 'MIT'
+	'path' => __FILE__,
+	'name' => 'EmbedVideo',
+	'author' => [
+		'Jim R. Wilson',
+		'Andrew Whitworth',
+		'Alexia E. Smith',
+		'...'
+		],
+	'url' => 'https://www.mediawiki.org/wiki/Extension:EmbedVideo',
+	'version' => EV_VERSION,
+	'descriptionmsg' => 'embedvideo_description',
+	'license-name' => 'MIT'
 ];
 
 /******************************************/
 /* Language Strings, Page Aliases, Hooks  */
 /******************************************/
-$extDir = __DIR__;
+$wgExtensionMessagesFiles['EmbedVideo']	= __DIR__ . '/EmbedVideo.i18n.php';
+$wgExtensionMessagesFiles['EmbedVideoMagic'] = __DIR__ . '/EmbedVideo.i18n.magic.php';
+$wgMessagesDirs['EmbedVideo'] = __DIR__ . '/i18n';
 
-$wgExtensionMessagesFiles['EmbedVideo']			= "{$extDir}/EmbedVideo.i18n.php";
-$wgExtensionMessagesFiles['EmbedVideoMagic']	= "{$extDir}/EmbedVideo.i18n.magic.php";
-$wgMessagesDirs['EmbedVideo']					= "{$extDir}/i18n";
+$wgAutoloadClasses['EmbedVideoHooks'] = __DIR__ . '/EmbedVideo.hooks.php';
+$wgAutoloadClasses['EmbedVideo\VideoService'] = __DIR__ . '/classes/VideoService.php';
+$wgAutoloadClasses['EmbedVideo\OEmbed']	= __DIR__ . '/classes/OEmbed.php';
 
-$wgAutoloadClasses['EmbedVideoHooks']			= "{$extDir}/EmbedVideo.hooks.php";
-$wgAutoloadClasses['EmbedVideo\VideoService']	= "{$extDir}/classes/VideoService.php";
-$wgAutoloadClasses['EmbedVideo\OEmbed']			= "{$extDir}/classes/OEmbed.php";
-
-$wgHooks['ParserFirstCallInit'][]				= 'EmbedVideoHooks::onParserFirstCallInit';
+$wgHooks['ParserFirstCallInit'][] = 'EmbedVideoHooks::onParserFirstCallInit';
 
 $wgResourceModules['ext.embedVideo'] = [
 	'localBasePath'	=> __DIR__,
 	'remoteExtPath'	=> 'EmbedVideo',
-	'styles'		=> ['css/embedvideo.css'],
-	'position'		=> 'top'
+	'styles' => ['css/embedvideo.css'],
+	'position' => 'top'
 ];
 
 if ( !isset( $wgEmbedVideoDefaultWidth ) && ( isset( $_SERVER['HTTP_X_MOBILE'] ) && $_SERVER['HTTP_X_MOBILE'] == 'true' ) && $_COOKIE['stopMobileRedirect'] != 1 ) {
