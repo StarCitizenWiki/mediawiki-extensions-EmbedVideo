e2f0b366b2528e222d279c258254e7e9ac540dfc
Alexia E. Smith <washuu@gmail.com>
2016-06-30 14:42:36 -0500

    Fix full screen problems.

--- js/autoResize.js
+++ js/autoResize.js
@@ -1,21 +1,31 @@
 (function(mw, $) {
-
-    $(function(){
+	$(function() {
         autoResizer(); // run first thing, because we dont need a resize to be broken.
 
         var autoResizerRTime;
         var autoResizerTimeout = false;
         var autoResizerDelta = 200;
+
         $(window).resize(function() {
-            autoResizerRTime = new Date();
+			autoResizerRTime = new Date();
             if (autoResizerTimeout === false) {
                 autoResizerTimeout = true;
                 setTimeout(autoResizerResizeEnd, autoResizerDelta);
             }
         });
 
+		function isFullscreen() {
+			if (document.fullscreenElement != null || document.mozFullScreenElement != null || document.webkitFullscreenElement != null) {
+				return true;
+			}
+			return false;
+		}
 
         function autoResizerResizeEnd() {
+			if (isFullscreen()) {
+                autoResizerTimeout = false;
+				return;
+			}
             if (new Date() - autoResizerRTime < autoResizerDelta) {
                 setTimeout(autoResizerResizeEnd, autoResizerDelta);
             } else {
@@ -32,24 +42,24 @@
                 var wrap = self.find('.embedvideowrap');
 
                 if (iframe.width() > parent.width()) {
-                    resizeHandler(self,iframe,parent,wrap);
+                    resizeHandler(self, iframe, parent, wrap);
                 } else {
-                    self.removeClass('autoResized').css('width','')
+                    self.removeClass('autoResized').css('width', '')
                     var originalWidth = iframe.attr("data-orig-width");
                     var originalHeight = iframe.attr("data-orig-height");
-                    iframe.width(originalWidth).css('width',originalWidth).attr('width',originalWidth);
-                    iframe.height(originalHeight).css('height',originalHeight).attr('height',originalHeight);
-                    wrap.width(originalWidth).css('width',originalWidth).attr('width',originalWidth);
-                    wrap.height(originalHeight).css('height',originalHeight).attr('height',originalHeight);
+                    iframe.width(originalWidth).css('width', originalWidth).attr('width', originalWidth);
+                    iframe.height(originalHeight).css('height', originalHeight).attr('height', originalHeight);
+                    wrap.width(originalWidth).css('width', originalWidth).attr('width', originalWidth);
+                    wrap.height(originalHeight).css('height', originalHeight).attr('height', originalHeight);
                 }
 
                 if (!self.hasClass('autoResized') && iframe.width() > parent.width()) {
-                    resizeHandler(self,iframe,parent,wrap);
+                    resizeHandler(self, iframe, parent, wrap);
                 }
             });
         }
 
-        function resizeHandler(self,iframe,parent,wrap) {
+        function resizeHandler(self, iframe, parent, wrap) {
             self.addClass('autoResized');
 
             if (typeof iframe.attr("data-orig-height") == 'undefined') {
@@ -61,11 +71,11 @@
             var newWidth = parent.width();
             var newHeight = newWidth / aspect;
 
-            self.width(newWidth).css('width',newWidth);
-            iframe.width(newWidth).css('width',newWidth).attr('width',newWidth);
-            iframe.height(newHeight).css('height',newHeight).attr('height',newHeight);
-            wrap.width(newWidth).css('width',newWidth).attr('width',newWidth);
-            wrap.height(newHeight).css('height',newHeight).attr('height',newHeight);
+            self.width(newWidth).css('width', newWidth);
+            iframe.width(newWidth).css('width', newWidth).attr('width', newWidth);
+            iframe.height(newHeight).css('height', newHeight).attr('height', newHeight);
+            wrap.width(newWidth).css('width', newWidth).attr('width', newWidth);
+            wrap.height(newHeight).css('height', newHeight).attr('height', newHeight);
         }
 
     });
