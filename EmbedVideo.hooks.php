StarCitizenWiki/mediawiki-extensions-EmbedVideo

commit 1c94e39c327bbecf91997706b510f9bfb3635383

Overhaul rendering and autoresize - fix center...again

EmbedVideo.hooks.php
@@ -268,17 +268,32 @@ class EmbedVideoHooks {
 	 */
 	static private function generateWrapperHTML( $html, $description = null, $addClass = null ) {
 		$classString = "embedvideo";
-
-		if ($addClass) {
-			$classString = $classString . " " . $addClass;
-		}
+		$styleString = "";
+		$innerClassString = "embedvideowrap";
 
 		if ( self::getContainer() == 'frame' ) {
-			$html = "<div class='thumb " . $classString . ( self::getAlignment() !== false ? " t" . self::getAlignment() : null ) . "'" . ( self::getAlignment() !== false ? "style='width: " . ( self::$service->getWidth() + 6 ) . "px;'" : null ) . "><div class='thumbinner' style='width: " . self::$service->getWidth() . "px;'>{$html}" . ( self::getDescription() !== false ? "<div class='thumbcaption'>" . self::getDescription() . "</div>" : null ) . "</div></div>";
-		} else {
-			// Normally I would avoid inline styles, but it is necessary in this case for center alignment as the stylesheet can not be dynamically modified.
-			$html = "<div class='" . $classString . " " . ( self::getAlignment() !== false ? " ev_" . self::getAlignment() : null ) . "' style='width: " . self::$service->getWidth() . "px;'>{$html}" . ( self::getDescription() !== false ? "<div class='thumbcaption'>" . self::getDescription() . "</div>" : null ) . "</div>";
+			$classString .= " thumb";
+			$innerClassString .= " thumbinner";
 		}
+
+		if (self::getAlignment() !== false) {
+			$classString .= " ev_" . self::getAlignment();
+			$styleString .= " width: " . ( self::$service->getWidth() + 6 ) . "px;'";
+		}
+
+		if ($addClass) {
+			$classString .= " " . $addClass;
+		}
+
+		//( self::getContainer() == 'frame' )
+
+		$html = "<div class='" . $classString . "' style='" . $styleString . "'>
+					<div class='" . $innerClassString . "' style='width: " . self::$service->getWidth() . "px;'>
+						{$html}
+						" . ( self::getDescription() !== false ? "<div class='thumbcaption'>" . self::getDescription() . "</div>" : null ) . "
+					</div>
+				</div>";
+
 		return $html;
 	}
 

css/embedvideo.css
@@ -1,7 +1,11 @@
-div.tcenter.embedvideo, div.embedvideo.ev_center {
+div.tcenter.embedvideo,
+div.embedvideo.ev_center  {
+	text-align: center;
+}
+
+div.embedvideo.ev_center > .embedvideowrap {
 	margin-left: auto;
 	margin-right: auto;
-	text-align: center;
 }
 
 div.embedvideo.ev_left {

js/autoResize.js
@@ -5,7 +5,7 @@
 
         var autoResizerRTime;
         var autoResizerTimeout = false;
-        var autoResizerDelta = 210;
+        var autoResizerDelta = 200;
         $(window).resize(function() {
             autoResizerRTime = new Date();
             if (autoResizerTimeout === false) {
@@ -29,24 +29,27 @@
                 var parent = $(this).parent();
                 var self = $(this);
                 var iframe = self.find('iframe');
+                var wrap = self.find('.embedvideowrap');
 
                 if (iframe.width() > parent.width()) {
-                    resizeHandler(self,iframe,parent);
+                    resizeHandler(self,iframe,parent,wrap);
                 } else {
                     self.removeClass('autoResized').css('width','')
                     var originalWidth = iframe.attr("data-orig-width");
                     var originalHeight = iframe.attr("data-orig-height");
                     iframe.width(originalWidth).css('width',originalWidth).attr('width',originalWidth);
                     iframe.height(originalHeight).css('height',originalHeight).attr('height',originalHeight);
+                    wrap.width(originalWidth).css('width',originalWidth).attr('width',originalWidth);
+                    wrap.height(originalHeight).css('height',originalHeight).attr('height',originalHeight);
                 }
 
                 if (!self.hasClass('autoResized') && iframe.width() > parent.width()) {
-                    resizeHandler(self,iframe,parent);
+                    resizeHandler(self,iframe,parent,wrap);
                 }
             });
         }
 
-        function resizeHandler(self,iframe,parent) {
+        function resizeHandler(self,iframe,parent,wrap) {
             self.addClass('autoResized');
 
             if (typeof iframe.attr("data-orig-height") == 'undefined') {
@@ -61,7 +64,8 @@
             self.width(newWidth).css('width',newWidth);
             iframe.width(newWidth).css('width',newWidth).attr('width',newWidth);
             iframe.height(newHeight).css('height',newHeight).attr('height',newHeight);
-
+            wrap.width(newWidth).css('width',newWidth).attr('width',newWidth);
+            wrap.height(newHeight).css('height',newHeight).attr('height',newHeight);
         }
 
     });

commit 2436ecd3d22739624a18b21e1b38edcb1ce57b6b

Fix extra apostrophe causing invalid HTML
Regression from 1c94e39.

EmbedVideo.hooks.php
@@ -308,7 +308,7 @@ class EmbedVideoHooks {
 
 		if (self::getAlignment() !== false) {
 			$classString .= " ev_" . self::getAlignment();
-			$styleString .= " width: " . ( self::$service->getWidth() + 6 ) . "px;'";
+			$styleString .= " width: " . ( self::$service->getWidth() + 6 ) . "px;";
 		}
 
 		if ($addClass) {