37c3ad51aee69f77e2cdefeddb8054e851b269a7
Hannes <hannes@octofox.de>
2021-06-08 11:20:15 +0200

    refactor: Fix ApiEmbedVideo.php

--- includes/ApiEmbedVideo.php
+++ includes/ApiEmbedVideo.php
@@ -19,18 +19,19 @@ class ApiEmbedVideo extends ApiBase {
 	 * Execute the API call.
 	 */
 	public function execute(): bool {
-		$getHTML = EmbedVideoHooks::parseEV(
-			null,
-			$this->getMain()->getVal( 'service' ),
-			$this->getMain()->getVal( 'id' ),
-			$this->getMain()->getVal( 'dimensions' ),
-			$this->getMain()->getVal( 'alignment' ),
-			$this->getMain()->getVal( 'description' ),
-			$this->getMain()->getVal( 'container' ),
-			$this->getMain()->getVal( 'urlargs' ),
-			$this->getMain()->getVal( 'autoresize' ),
-			$this->getMain()->getVal( 'valignment' )
-		);
+		$ev = new EmbedVideo(null, [
+			sprintf('service=%s', $this->getMain()->getVal( 'service' )),
+			sprintf('id=%s', $this->getMain()->getVal( 'id' )),
+			sprintf('dimensions=%s', $this->getMain()->getVal( 'dimensions' )),
+			sprintf('alignment=%s', $this->getMain()->getVal( 'alignment' )),
+			sprintf('description=%s', $this->getMain()->getVal( 'description' )),
+			sprintf('container=%s', $this->getMain()->getVal( 'container' )),
+			sprintf('urlargs=%s', $this->getMain()->getVal( 'urlargs' )),
+			sprintf('autoresize=%s', $this->getMain()->getVal( 'autoresize' )),
+			sprintf('valignment=%s', $this->getMain()->getVal( 'valignment' )),
+		]);
+
+		$getHTML = $ev->output();
 
 		if ( is_array( $getHTML ) ) {
 			$HTML = $getHTML[0];
