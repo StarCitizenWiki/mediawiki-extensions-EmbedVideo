c501e0ad64c1e10c0ab0d0c3861f97c149a09221
Jerimee Richir <jerimee.richir@gmail.com>
2021-11-08 17:21:46 -0500

    crude revert take 2

--- includes/EmbedService/EmbedServiceFactory.php
+++ includes/EmbedService/EmbedServiceFactory.php
@@ -4,97 +4,92 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\EmbedVideo\EmbedService;
 
-use InvalidArgumentException;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Spotify\SpotifyAlbum;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Spotify\SpotifyArtist;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Spotify\SpotifyTrack;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Twitch\Twitch;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Twitch\TwitchClip;
-use MediaWiki\Extension\EmbedVideo\EmbedService\Twitch\TwitchVod;
-use MediaWiki\Extension\EmbedVideo\EmbedService\YouTube\YouTube;
-use MediaWiki\Extension\EmbedVideo\EmbedService\YouTube\YouTubeOEmbed;
-use MediaWiki\Extension\EmbedVideo\EmbedService\YouTube\YouTubePlaylist;
-use MediaWiki\Extension\EmbedVideo\EmbedService\YouTube\YouTubeVideoList;
+use ConfigException;
+use Exception;
+use MediaWiki\Extension\EmbedVideo\OEmbed;
+use MediaWiki\MediaWikiServices;
+use UnexpectedValueException;
 
-final class EmbedServiceFactory {
-
+final class EmbedHtmlFormatter {
 	/**
-	 * List of all available services
+	 * Generates the iframe html
 	 *
-	 * @var AbstractEmbedService[]
+	 * @param AbstractEmbedService $service
+	 * @return string
 	 */
-	private static $availableServices = [
-		ArchiveOrg::class,
-		SoundCloud::class,
-		SpotifyAlbum::class,
-		SpotifyArtist::class,
-		SpotifyTrack::class,
-		Twitch::class,
-		TwitchClip::class,
-		TwitchVod::class,
-		Vimeo::class,
-		YouTubeOEmbed::class,
-		YouTube::class,
-		YouTubePlaylist::class,
-		YouTubeVideoList::class,
-	];
+	public static function toHtml( AbstractEmbedService $service ): string {
+		if ( $service instanceof OEmbedServiceInterface ) {
+			try {
+				$data = OEmbed::newFromRequest( $service->getUrl() );
+				return $data->getHtml();
+			} catch ( UnexpectedValueException $e ) {
+				return $e->getMessage();
+			}
+		}
+
+		$attributes = $service->getIframeAttributes();
+		$attributes['width'] = $service->getWidth();
+		$attributes['height'] = $service->getHeight();
+
+		$srcType = 'src';
+		try {
+			$consent = MediaWikiServices::getInstance()
+				->getConfigFactory()
+				->makeConfig( 'EmbedVideo' )
+				->get( 'EmbedVideoRequireConsent' );
+			if ( $consent === true ) {
+				$srcType = 'data-src';
+			}
+		} catch ( ConfigException $e ) {
+			//
+		}
+
+		$attributes[$srcType] = $service->getUrl();
+
+		$out = array_map( static function ( $key, $value ) {
+			return sprintf( '%s="%s"', $key, $value );
+		}, array_keys( $attributes ), $attributes );
+
+		return sprintf( '<iframe %s></iframe>', implode( ' ', $out ) );
+	}
 
 	/**
-	 * @param string $serviceName
-	 * @param string $id
-	 * @return AbstractEmbedService
+	 * Generates the html used for embed thumbnails
+	 *
+	 * @param AbstractEmbedService $service
+	 * @return string The final html (can be empty on error or missing data)
 	 */
-	public static function newFromName( string $serviceName, string $id ): AbstractEmbedService {
-		switch ( strtolower( $serviceName ) ) {
-			case 'archiveorg':
-				return new ArchiveOrg( $id );
+	public static function makeThumbHtml( AbstractEmbedService $service ): string {
+		if ( $service->getLocalThumb() === null ) {
+			return "";
+		}
 
-			case 'soundcloud':
-				return new SoundCloud( $id );
+		try {
+			$url = wfExpandUrl( $service->getLocalThumb()->getUrl() );
 
-			case 'spotifyalbum':
-				return new SpotifyAlbum( $id );
-
-			case 'spotifyartist':
-				return new SpotifyArtist( $id );
-
-			case 'spotify':
-			case 'spotifytrack':
-				return new SpotifyTrack( $id );
-
-			case 'twitch':
-				return new Twitch( $id );
-
-			case 'twitchclip':
-				return new TwitchClip( $id );
-
-			case 'twitchvod':
-				return new TwitchVod( $id );
-
-			case 'vimeo':
-				return new Vimeo( $id );
-
-			case 'youtubeoembed':
-				return new YouTubeOEmbed( $id );
-
-			case 'youtube':
-				return new YouTube( $id );
-
-			case 'youtubeplaylist':
-				return new YouTubePlaylist( $id );
-
-			case 'youtubevideolist':
-				return new YouTubeVideoList( $id );
-
-			default:
-				throw new InvalidArgumentException( sprintf( 'VideoService "%s" not recognized.', $serviceName ) );
+			// phpcs:disable
+			return <<<HTML
+<picture class="embedvideo-consent__thumbnail"><!--
+	--><img src="{$url}" loading="lazy" class="embedvideo-consent__thumbnail__image" alt="Thumbnail for {$service->getTitle()}"/><!--
+--></picture>
+HTML;
+			// phpcs:enable
+		} catch ( Exception $e ) {
+			return "";
 		}
 	}
 
 	/**
-	 * @return AbstractEmbedService[]
+	 * Generates the html used for embed titles
+	 *
+	 * @param AbstractEmbedService $service
+	 * @return string The final html (can be empty on error or missing data)
 	 */
-	public static function getAvailableServices(): array {
-		return self::$availableServices;
+	public static function makeTitleHtml( AbstractEmbedService $service ): string {
+		if ( $service->getTitle() === null ) {
+			return "";
+		}
+
+		return sprintf( '<div class="embedvideo-consent__title">%s</div>', $service->getTitle() );
 	}
 }
