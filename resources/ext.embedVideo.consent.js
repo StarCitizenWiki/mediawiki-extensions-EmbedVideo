3a3f2d806cf1f260f71018d0cca0683c4ec1002c
Hannes <hannes@octofox.de>
2022-10-17 09:46:58 +0200

    refactor: Update oembed calls
    Add an optional config dict for accessing info endpoint data
    
    Soundcloud Oembed seems fine, tested with https://soundcloud.com/oembed?format=json&url=https://soundcloud.com/turbokevin/turbokevin-chase-the-sun

--- resources/ext.embedVideo.consent.js
+++ resources/ext.embedVideo.consent.js
@@ -1,11 +1,22 @@
 (function () {
 	const fetchThumb = async (url, parent, outerDiv) => {
 		let callUrl;
+		/**
+		 * An optional configuration dict for accessing data from the 'info' endpoint for titles and thumbnails
+		 * @type {{dataKey: null, titleKey: string, thumbnailKey: string}}
+		 */
+		const dataConfig = {
+			'dataKey': null,
+			'titleKey': 'title',
+			'thumbnailKey': 'thumbnail_url',
+		}
 
 		switch( outerDiv.getAttribute('data-service') ) {
 			case 'bilibili':
 				// Not Oembed
-				//callUrl = 'https://api.bilibili.com/x/web-interface/view?bvid=';
+				callUrl = 'https://api.bilibili.com/x/web-interface/view?bvid=';
+				dataConfig['dataKey'] = 'data';
+				dataConfig['thumbnailKey'] = 'pic';
 				break;
 			case 'niconico':
 				// Not Oembed
@@ -13,9 +24,8 @@
 				//callUrl = 'https://ext.nicovideo.jp/api/getthumbinfo/';
 				break;
 			case 'soundcloud':
-				// Oembed
-				// The soundcloud URL is messed up,  maybe we should look for another way to call API
-				// callUrl = 'https://soundcloud.com/oembed?format=json&url=';
+				callUrl = 'https://soundcloud.com/oembed?format=json&url=';
+				break;
 			case 'spotifyalbum':
 				callUrl = 'https://open.spotify.com/oembed?url=https://open.spotify.com/album/';
 				break;
@@ -36,7 +46,7 @@
 		}
 
 		// Some url manipulation foo which tries to get the id of the requested video
-		if (url.substr(0, 1) === '/') {
+		if (url.substring(0, 1) === '/') {
 			url = 'http:' + url;
 		}
 
@@ -52,8 +62,8 @@
 			return;
 		}
 
-		if (id.substr(-1) === '?') {
-			id = id.substr(0, id.length - 1)
+		if (id.substring(-1) === '?') {
+			id = id.substring(0, id.length - 1)
 		}
 
 		// Do the actual fetch
@@ -65,26 +75,30 @@
 				return result.json();
 			})
 			.then(json => {
-				if (typeof json.thumbnail_url === 'undefined' || parent.querySelectorAll('.embedvideo-consent__thumbnail').length > 0) {
+				if (dataConfig.dataKey !== null) {
+					json = json[dataConfig.dataKey];
+				}
+
+				if (typeof json[dataConfig.thumbnailKey] === 'undefined' || parent.querySelectorAll('.embedvideo-consent__thumbnail').length > 0) {
 					return;
 				}
 
-				const 
+				const
 					overlay = parent.querySelector('.embedvideo-consent__overlay'),
 					picture = document.createElement('picture'),
 					image = document.createElement('img');
 
 				picture.classList.add('embedvideo-consent__thumbnail');
-				image.src = json.thumbnail_url;
+				image.src = json[dataConfig.thumbnailKey];
 				image.setAttribute('loading', 'lazy');
 				image.classList.add('embedvideo-consent__thumbnail__image');
 				picture.append(image);
 				parent.prepend(picture);
 
-				if (typeof json.title !== 'undefined' && json.title.length > 0) {
+				if (typeof json[dataConfig.titleKey] !== 'undefined' && json[dataConfig.titleKey].length > 0) {
 					const title = document.createElement('div');
 					title.classList.add('embedvideo-consent__title');
-					title.innerText = json.title;
+					title.innerText = json[dataConfig.titleKey];
 					overlay.classList.add('embedvideo-consent__overlay--hastitle');
 					overlay.prepend(title);
 				}
