7cbbd64b5367b2d36653edb250d5d8edca526bcf
gaon12 <34927797+gaon12@users.noreply.github.com>
2022-10-25 21:48:28 +0900

    Add NaverTV, KakaoTV

---
+++ includes/EmbedService/KakaoTV.php
@@ -0,0 +1,62 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\EmbedVideo\EmbedService;
+
+final class KakaoTV extends AbstractEmbedService {
+	/**
+	 * @inheritDoc
+	 */
+	public function getBaseUrl(): string {
+		return '//play-tv.kakao.com/embed/player/cliplink/%1$s';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getAspectRatio(): ?float {
+		return 16 / 9;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getDefaultWidth(): int {
+		return 640;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getDefaultHeight(): int {
+		return 360;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getUrlRegex(): array {
+		return [
+			'#play-tv.kakao\.com/embed/player/cliplink/((?:[a-zA-Z]{2})?[\d]+)#is',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getIdRegex(): array {
+		return [
+			'#^((?:[a-zA-Z]{2})?[\d]+)$#is',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCSPUrls(): array {
+		return [
+			'https://tv.kakao.com',
+		];
+	}
+}
---
+++ includes/EmbedService/NaverTV.php
@@ -0,0 +1,62 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\EmbedVideo\EmbedService;
+
+final class NaverTV extends AbstractEmbedService {
+	/**
+	 * @inheritDoc
+	 */
+	public function getBaseUrl(): string {
+		return '//tv.naver.com/embed/%1$s';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getAspectRatio(): ?float {
+		return 16 / 9;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getDefaultWidth(): int {
+		return 640;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getDefaultHeight(): int {
+		return 360;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getUrlRegex(): array {
+		return [
+			'#tv.naver\.com/embed/((?:[a-zA-Z]{2})?[\d]+)#is',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getIdRegex(): array {
+		return [
+			'#^((?:[a-zA-Z]{2})?[\d]+)$#is',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCSPUrls(): array {
+		return [
+			'https://tv.naver.com',
+		];
+	}
+}
