adaf709d87df6e4be24eb6fcabc2aa4848940030
H. C. Kruse <hannes@octofox.de>
2023-06-27 09:56:03 +0200

    fix(FFProbe): Fix loading metadata from cache
    Fixes #59

--- includes/Media/FFProbe/FFProbe.php
+++ includes/Media/FFProbe/FFProbe.php
@@ -16,202 +16,207 @@ use MediaWiki\ShellDisabledError;
 use Wikimedia\LightweightObjectStore\ExpirationAwareness;
 
 class FFProbe {
-	/**
-	 * MediaWiki File
-	 *
-	 * @var File|FSFile
-	 */
-	private $file;
+    /**
+     * MediaWiki File
+     *
+     * @var File|FSFile
+     */
+    private $file;
 
-	/**
-	 * Meta Data Cache
-	 *
-	 * @var array
-	 */
-	private $metadata;
+    /**
+     * Meta Data Cache
+     *
+     * @var array
+     */
+    private $metadata;
 
-	/**
-	 * Main Constructor
-	 *
-	 * @param File|FSFile $file MediaWiki File
-	 * @return void
-	 */
-	public function __construct( $file ) {
-		$this->file = $file;
-	}
+    /**
+     * Main Constructor
+     *
+     * @param File|FSFile $file MediaWiki File
+     * @return void
+     */
+    public function __construct( $file ) {
+        $this->file = $file;
+    }
 
-	/**
-	 * Return the entire cache of metadata.
-	 *
-	 * @param string $select The selected audio/video stream
-	 * @return array Meta Data
-	 */
-	public function getMetaData( string $select = 'v:0' ): array {
-		if ( $this->file instanceof FSFile ) {
-			$cacheKey = $this->file->getSha1Base36();
-		} else {
-			$cacheKey = $this->file->getSha1();
-		}
+    /**
+     * Return the entire cache of metadata.
+     *
+     * @param string $select The selected audio/video stream
+     * @return bool Flag if loading did succeed
+     */
+    public function loadMetaData(string $select = 'v:0' ): bool {
+        if ( $this->file instanceof FSFile ) {
+            $cacheKey = $this->file->getSha1Base36();
+        } else {
+            $cacheKey = $this->file->getSha1();
+        }
 
-		$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
-		$cacheKey = $cache->makeGlobalKey( 'EmbedVideo', 'ffprobe', $cacheKey, $select );
+        $cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+        $cacheKey = $cache->makeGlobalKey( 'EmbedVideo', 'ffprobe', $cacheKey, $select );
 
-		$result = $cache->getWithSetCallback(
-			$cacheKey,
-			// FSFiles are usually only present for uploads(?), only "real" files are relevant
-			$this->file instanceof File ? ExpirationAwareness::TTL_INDEFINITE : ExpirationAwareness::TTL_MINUTE,
-			function ( $old, &$ttl ) {
-				if ( $this->file instanceof FSFile ) {
-					$title = 'Newly uploaded file';
-				} else {
-					$title = $this->file->getTitle();
-					$title = $title !== null ? $title->getBaseText() : 'Untitled file';
-				}
+        $result = $cache->getWithSetCallback(
+            $cacheKey,
+            // FSFiles are usually only present for uploads(?), only "real" files are relevant
+            $this->file instanceof File ? ExpirationAwareness::TTL_INDEFINITE : ExpirationAwareness::TTL_MINUTE,
+            function ( $old, &$ttl ) {
+                if ( $this->file instanceof FSFile ) {
+                    $title = 'Newly uploaded file';
+                } else {
+                    $title = $this->file->getTitle();
+                    $title = $title !== null ? $title->getBaseText() : 'Untitled file';
+                }
 
-				wfDebugLog(
-					'EmbedVideo',
-					sprintf( 'Writing FFProbe Cache for %s', $title )
-				);
+                wfDebugLog(
+                    'EmbedVideo',
+                    sprintf( 'Writing FFProbe Cache for %s', $title )
+                );
 
-				$result = $this->invokeFFProbe();
+                $result = $this->invokeFFProbe();
 
-				if ( $result === false ) {
-					$ttl = ExpirationAwareness::TTL_UNCACHEABLE;
-					return $old;
-				}
+                if ( $result === false ) {
+                    $ttl = ExpirationAwareness::TTL_UNCACHEABLE;
+                    return $old;
+                }
 
-				return $this->metadata;
-			}
-		);
+                return $this->metadata;
+            }
+        );
 
-		if ( is_array( $result ) ) {
-			return $result;
-		}
+        if ( is_array( $result ) ) {
+            $this->metadata = [
+                'streams' => $result['streams'],
+                'format' => $result['format'],
+            ];
 
-		return [];
-	}
+            return true;
+        }
 
-	/**
-	 * Get a selected stream.  Follows ffmpeg's stream selection style.
-	 *
-	 * @param string $select Stream identifier
-	 * Examples:
-	 *		"v:0" - Select the first video stream
-	 * 		"a:1" - Second audio stream
-	 * 		"i:0" - First stream, whatever it is.
-	 * 		"s:2" - Third subtitle
-	 * 		"d:0" - First generic data stream
-	 * 		"t:1" - Second attachment
-	 * @return false|StreamInfo StreamInfo object or false if does not exist.
-	 */
-	public function getStream( string $select ) {
-		$this->getMetaData( $select );
+        return false;
+    }
 
-		$types = [
-			'v'	=> 'video',
-			'a'	=> 'audio',
-			'i'	=> false,
-			's'	=> 'subtitle',
-			'd'	=> 'data',
-			't'	=> 'attachment'
-		];
+    /**
+     * Get a selected stream.  Follows ffmpeg's stream selection style.
+     *
+     * @param string $select Stream identifier
+     * Examples:
+     *		"v:0" - Select the first video stream
+     * 		"a:1" - Second audio stream
+     * 		"i:0" - First stream, whatever it is.
+     * 		"s:2" - Third subtitle
+     * 		"d:0" - First generic data stream
+     * 		"t:1" - Second attachment
+     * @return false|StreamInfo StreamInfo object or false if does not exist.
+     */
+    public function getStream( string $select ) {
+        $this->loadMetaData( $select );
 
-		if ( !isset( $this->metadata['streams'] ) ) {
-			return false;
-		}
+        $types = [
+            'v'	=> 'video',
+            'a'	=> 'audio',
+            'i'	=> false,
+            's'	=> 'subtitle',
+            'd'	=> 'data',
+            't'	=> 'attachment'
+        ];
 
-		[ $type, $index ] = explode( ":", $select );
-		$index = (int)$index;
+        if ( !isset( $this->metadata['streams'] ) ) {
+            return false;
+        }
 
-		$type = ( $types[$type] ?? false );
+        [ $type, $index ] = explode( ":", $select );
+        $index = (int)$index;
 
-		$i = 0;
-		foreach ( $this->metadata['streams'] as $stream ) {
-			if ( $type !== false && isset( $stream['codec_type'] ) ) {
-				if ( $index === $i && $stream['codec_type'] === $type ) {
-					return new StreamInfo( $stream );
-				}
-			}
-			if ( $type === false || $stream['codec_type'] === $type ) {
-				$i++;
-			}
-		}
-		return false;
-	}
+        $type = ( $types[$type] ?? false );
 
-	/**
-	 * Get the FormatInfo object.
-	 *
-	 * @return false|FormatInfo FormatInfo object or false if does not exist.
-	 */
-	public function getFormat() {
-		$this->getMetaData();
+        $i = 0;
+        foreach ( $this->metadata['streams'] as $stream ) {
+            if ( $type !== false && isset( $stream['codec_type'] ) ) {
+                if ( $index === $i && $stream['codec_type'] === $type ) {
+                    return new StreamInfo( $stream );
+                }
+            }
+            if ( $type === false || $stream['codec_type'] === $type ) {
+                $i++;
+            }
+        }
+        return false;
+    }
 
-		if ( !isset( $this->metadata['format'] ) ) {
-			return false;
-		}
+    /**
+     * Get the FormatInfo object.
+     *
+     * @return false|FormatInfo FormatInfo object or false if does not exist.
+     */
+    public function getFormat() {
+        $this->loadMetaData();
 
-		return new FormatInfo( $this->metadata['format'] );
-	}
+        if ( !isset( $this->metadata['format'] ) ) {
+            return false;
+        }
 
-	/**
-	 * @return bool|string
-	 */
-	private function getFilePath() {
-		if ( $this->file instanceof FSFile ) {
-			return $this->file->getPath();
-		}
+        return new FormatInfo( $this->metadata['format'] );
+    }
 
-		return $this->file->getLocalRefPath();
-	}
+    /**
+     * @return bool|string
+     */
+    private function getFilePath() {
+        if ( $this->file instanceof FSFile ) {
+            return $this->file->getPath();
+        }
 
-	/**
-	 * Invoke ffprobe on the command line.
-	 *
-	 * @return bool Success
-	 */
-	private function invokeFFProbe(): bool {
-		try {
-			$ffprobeLocation = MediaWikiServices::getInstance()
-				->getConfigFactory()
-				->makeConfig( 'EmbedVideo' )
-				->get( 'FFProbeLocation' );
-		} catch ( ConfigException $e ) {
-			return false;
-		}
+        return $this->file->getLocalRefPath();
+    }
 
-		if ( Shell::isDisabled() || $ffprobeLocation === false || !file_exists( $ffprobeLocation ) ) {
-			$this->metadata = [];
-			return false;
-		}
+    /**
+     * Invoke ffprobe on the command line.
+     *
+     * @return bool Success
+     */
+    private function invokeFFProbe(): bool {
+        try {
+            $ffprobeLocation = MediaWikiServices::getInstance()
+                ->getConfigFactory()
+                ->makeConfig( 'EmbedVideo' )
+                ->get( 'FFProbeLocation' );
+        } catch ( ConfigException $e ) {
+            return false;
+        }
 
-		$command = Shell::command( $ffprobeLocation );
+        if ( Shell::isDisabled() || $ffprobeLocation === false || !file_exists( $ffprobeLocation ) ) {
+            $this->metadata = [];
+            return false;
+        }
 
-		$command->unsafeParams( [
-			'-v quiet',
-			'-print_format json',
-			'-show_format',
-			'-show_streams',
-			Shell::escape( $this->getFilePath() ),
-		] );
+        $command = Shell::command( $ffprobeLocation );
 
-		try {
-			$result = $command->execute();
+        $command->unsafeParams( [
+            '-v quiet',
+            '-print_format json',
+            '-show_format',
+            '-show_streams',
+            Shell::escape( $this->getFilePath() ),
+        ] );
 
-			$json = json_decode( $result->getStdout(), true, 512, JSON_THROW_ON_ERROR );
-		} catch ( Exception | JsonException | ShellDisabledError | ProcOpenError $e ) {
-			wfLogWarning( $e->getMessage() );
-			$this->metadata = [];
-			return false;
-		}
+        try {
+            $result = $command->execute();
 
-		if ( is_array( $json ) ) {
-			$this->metadata = $json;
-		} else {
-			$this->metadata = [];
-			return false;
-		}
+            $json = json_decode( $result->getStdout(), true, 512, JSON_THROW_ON_ERROR );
+        } catch ( Exception | JsonException | ShellDisabledError | ProcOpenError $e ) {
+            wfLogWarning( $e->getMessage() );
+            $this->metadata = [];
+            return false;
+        }
 
-		return true;
-	}
+        if ( is_array( $json ) ) {
+            $this->metadata = $json;
+        } else {
+            $this->metadata = [];
+            return false;
+        }
+
+        return true;
+    }
 }
